855c35e1042a3479fec4cad5ef6d8bf4cacda069
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-01-22 08:47:45 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,29 @@
 class Translation(object):
 
       # Start text
-      START = "pm not alowed hire @ishan_404"             
+      START = """<b>Hi 👋🏻 Bro,
+
+I'm UK Studios Official a Bot to Maintain Your Channels. I am very useful for the Channel Admin who have many Channels.
+
+See help for more Details.
+
+Maintained By : [Karthik](https://t.me/HMTD_Karthik)</b>"""             
+
+      # Help text
+      HELP = """<b>The Ultimate Tool to Automate Custom Telegram Message Forwarding. Live Syncer, Auto Poster, Backup Bot, Cloner, Chat Forwarder, Duplicator, ... Call it whatever you like! tgcf can fulfill Your Custom Needs.</b>"""
 
       #About text
       ABOUT = """
-<b>📝 Language:</b> Python 3
-<b>🧰 Framework:</b> Pyrogram
-<b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
-<b>📢 Channel:</b> NS BOT UPDATES
-<b>👥 Group:</b> Ns BOT SUPPORT
-<b>🌐Source Code:</b> Press Me 🥰"""
+<b>🤖 My Name : [UK Auto Forward Bot](https://t.me/UK_Auto_Forward_Bot)
+
+🧑🏻‍💻 Developer : [Karthik](https://t.me/HMTD_Karthik)
+
+📝 Language : Pyrogram
+
+📚 Framework : Python3
+
+📡 Hosted on : VPS
+
+📢 Updates Channel : [UK Movies Zone (Updates)](https://t.me/UK_Movies_Zone_Updates)
+
+🌐 Website : [www.HMTDMovies.tk](https://www.HMTDMovies.tk)</b>"""
